4d82b1970e8dee5e8b46b77cfa91ce9414cb07a4
Bogdan <bogdasafronov@gmail.com>
2023-03-21 06:36:47 +0300

    test with connection to db

--- src/modules/_app/app.module.ts
+++ src/modules/_app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://bogdasafronov:XKHWy2TV9vm1iI9V@twinkle-main.ugrqhav.mongodb.net/test`,
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,25 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
