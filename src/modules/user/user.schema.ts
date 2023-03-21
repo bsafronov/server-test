6c894f6ed9f0f4ecd8c218043a20f90a58c54559
Bogdan <bogdasafronov@gmail.com>
2023-03-21 07:32:24 +0300

    add up folder

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -6,7 +6,7 @@ export type UserDocument = HydratedDocument<User>;
 @Schema({
   timestamps: true,
 })
-export class User {
+export class User extends BaseSchema {
   @Prop({ required: true, unique: true })
   username: string;
 
