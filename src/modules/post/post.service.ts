e5a5fd4455a625b1b147630332e47ee2143a236c
Bogdan <bogdasafronov@gmail.com>
2023-03-21 07:12:40 +0300

    folder up test

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
-import { Poster, PosterDocument } from './schemas/Post.schema';
+import { Poster, PosterDocument } from './post.schema';
 
 @Injectable()
 export class PostService {
   constructor(
-    @InjectModel(Poster.name) private postModel: Model<PosterDocument>, // @InjectModel(PostComment.name)
-  ) // private postCommentModel: Model<PostCommentDocument>,
-  // @InjectModel(PostLike.name) private postLikeModel: Model<PostLikeDocument>,
+    @InjectModel(Poster.name) private postModel: Model<PosterDocument>, // @InjectModel(PostComment.name) // private postCommentModel: Model<PostCommentDocument>,
+  ) // @InjectModel(PostLike.name) private postLikeModel: Model<PostLikeDocument>,
   {}
 }
